8633e96888930fa43563b5801466bf93d379dfe9
Björn Platte <lonoxX96@gmail.com>
2023-07-08 15:26:26 +0200

    add: CI to build and deploy

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.html',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.html$/,
+        use: 'html-loader',
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+    new CopyPlugin({
+      patterns: [
+        { from: 'src/assets/img', to: 'assets/img' },
+      ],
+    }),
+  ],
+};
